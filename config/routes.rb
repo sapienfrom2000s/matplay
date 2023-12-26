d5a8369ace67aac5a3b84df3603cc4d56979875f
brickwall <sk292207@gmail.com>
2023-12-26 09:15:25 +0530

    Customize routes
    - add template route to fill in matrix values

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :multiplications
+  resources :multiplications, only: [:index, :new]
+  get 'multiplication/template', to: 'multiplications#template'
+  get 'multiplication/result', to: 'multiplications#result'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
