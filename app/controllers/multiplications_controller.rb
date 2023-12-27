4294b405fe96ba8538b1b2dc9a1f16ec2a37c3d9
brickwall <sk292207@gmail.com>
2023-12-27 11:47:40 +0530

    clean up

--- app/controllers/multiplications_controller.rb
+++ app/controllers/multiplications_controller.rb
@@ -1,8 +1,8 @@
 class MultiplicationsController < ApplicationController
-  # GET /multiplications or /multiplications.json
   def index; end
 
-  # GET /multiplications/1 or /multiplications/1.json
+  def new; end
+
   def template
     @row_a = matrix_params['row-A'].to_i
     @row_b = matrix_params['row-B'].to_i
