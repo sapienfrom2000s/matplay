0d0dc6e6212d236a28cab8af854e8e1330d6d163
brickwall <sk292207@gmail.com>
2023-12-27 12:43:03 +0530

    Use POST instead of GET for form submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :multiplications, only: [:index, :new]
-  get 'multiplication/template', to: 'multiplications#template'
-  get 'multiplication/result', to: 'multiplications#result'
+  post 'multiplication/template', to: 'multiplications#template'
+  post 'multiplication/result', to: 'multiplications#result'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
